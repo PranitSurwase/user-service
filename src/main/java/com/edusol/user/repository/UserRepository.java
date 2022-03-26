78cc8b57e9f3db6975319ddc5db34c3a6378cbf3
PranitSurwase <42385630+PranitSurwase@users.noreply.github.com>
2022-03-26 09:18:33 +0530

    first commit

---
+++ src/main/java/com/edusol/user/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.edusol.user.repository;
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edusol.user.model.User;
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+ List<User> findByCity(String city);
+List<User> findByEmail(String email);
+List<User> findByName(String email);
+}
---
+++ src/main/java/com/edusol/user/service/UserService.java
@@ -0,0 +1,88 @@
+package com.edusol.user.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.edusol.user.model.User;
+import com.edusol.user.repository.UserRepository;
+
+@Service
+public class UserService {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	
+	private UserRepository  userRepository;
+	public User addUser(User user) {
+		
+		userRepository.save(user);
+		logger.info(user.toString());
+		return user;
+	}
+	public List<User > getUser()
+	{
+		List<User > user=userRepository.findAll();
+				logger.info(user.toString());
+		 return user;
+	}
+	public String updateUser(User user) {
+		logger.info(user.toString());
+		userRepository.save(user);
+		logger.info("record updated successfully");
+		return "record has been updated";
+		
+	}
+	public ResponseEntity<String> deleteUser(int id) {
+		logger.info("record deleted successfully"+id);
+		String message="";
+		try {
+			User user=userRepository.getOne(id);
+		
+		userRepository.deleteById(id);;;
+		logger.info(message);
+		return new ResponseEntity<String>(message,HttpStatus.OK);
+		}catch (Exception e) {
+			logger.error("not found");
+			logger.error(message);
+			return new ResponseEntity<String> (message,HttpStatus.NOT_FOUND);
+			
+		}
+	
+	}
+	public List<User> getUsersByCity(String city) {
+		List<User> users=userRepository.findByCity(city);
+		return users;
+	}
+
+	/*
+	 * public List<User> getUsersByCity(String city) {
+	 * logger.info("find by city"+city); List<User>
+	 * users=userRepository.findByCity(city); return users;
+	 * }
+	 */
+	
+	
+	
+	public List<User> getUsersByEmail(String email) {
+		logger.info("find by email" + email);
+		List<User> users = userRepository.findByEmail(email);
+		return users;
+	}
+	public List<User> getUsersByName(String name) {
+		logger.info("find by email" + name);
+		List<User> users = userRepository.findByName(name);
+		return users;
+	}
+	
+	/*
+	 * public List<User> getUsersByCity(String city) {
+	 * logger.info("Getting user details:"+city); List<User>
+	 * user=userRepository.findByCity(city); return user; }
+	 */
+
+}
